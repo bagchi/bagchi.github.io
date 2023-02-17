f4bb052f80637665f7ab75b5e99de8f0a923b01e
Leon <leon.yee000@gmail.com>
2023-02-17 01:18:48 -0500

    initial video-page update commit

---
+++ videos-page.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "videos.csv",
+        dataType: "text",
+        success: function (data) {
+            processData(data);
+        }
+    });
+});
+
+function processData(allText) {
+    allTextLines = allText.split("\n");
+    allTextLines.shift();
+    for(var i = 0; i < allTextLines.length; i++) { // CSV is title, date, link, description
+        var data = allTextLines[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+
+        if(i == 0) {
+            document.getElementById("video-list").innerHTML += '<div class="carousel-item active">\n<iframe class="d-block w-100" src=' + data[2] + '></iframe>\n</div>\n'
+        }
+        document.getElementById("video-list").innerHTML += '<div class="carousel-item">\n<img class="d-block w-100" src=' + data[2] + '></img>\n</div>\n'
+        console.log(data);
+    }
+
+}
